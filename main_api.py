4ce9d8b6eaf5fd9f68e8fc388486de1b8bfc86d0
sem_samsum <128489573+sem-samsum1721@users.noreply.github.com>
2025-08-14 00:13:59 +0300

    Update main_api.py

--- main_api.py
+++ main_api.py
@@ -5,32 +5,32 @@ from fastapi.responses import JSONResponse
 from PIL import Image
 import io
 
-# Tıpkı ilk script'teki gibi, modeli ve gerekli araçları import ediyoruz
+#  modeli ve gerekli araçları import ediyoruz
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
 from tensorflow.keras.models import Model
 
-# İki vektör arasındaki benzerliği hesaplamak için
+# benzerlik hesaplama
 from sklearn.metrics.pairwise import cosine_similarity
 
 # --- UYGULAMA BAŞLANGICI ---
 
 print("FastAPI uygulaması başlatılıyor...")
 
-# 1. FastAPI uygulamasını oluştur
+#  FastAPI uygulamasını oluşturma
 app = FastAPI(title="Akıllı Görsel Ürün Arama API")
 
-# 2. Önceden eğitilmiş modeli yükle (ilk script'tekiyle aynı)
+#eğitilmiş modeli yükle 
 base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling='avg')
 print("Yapay zeka modeli belleğe yüklendi.")
 
-# 3. Oluşturduğumuz JSON veritabanını yükle
+# JSON veritabanını yükleme
 DATABASE_PATH = 'urun_veritabani/urun_veritabani.json'
 with open(DATABASE_PATH, 'r', encoding='utf-8') as f:
     database = json.load(f)
 print(f"Ürün veritabanı '{DATABASE_PATH}' dosyasından yüklendi. {len(database)} ürün bulundu.")
 
 
-# --- YARDIMCI FONKSİYONLAR ---
+
 
 def preprocess_image(img_bytes: bytes):
     """Gelen resmi modele uygun formata getirir."""
@@ -52,13 +52,13 @@ async def find_similar_products(image_file: UploadFile = File(...)):
     Yüklenen bir görsele en çok benzeyen ürünleri veritabanından bulur.
     """
     try:
-        # Yüklenen dosyanın içeriğini (byte olarak) oku
+        
         contents = await image_file.read()
         
-        # Resmi ön işleme fonksiyonundan geçir
+        #  ön işleme
         processed_image = preprocess_image(contents)
         
-        # Yüklenen resmin özellik vektörünü model ile çıkar
+        #  özellik vektörünü model ile çıkar
         query_vector = base_model.predict(processed_image).flatten()
         
         results = []
